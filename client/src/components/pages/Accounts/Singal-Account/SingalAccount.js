d45a6a7fa5746763d445dfb9c7ed229655a6a2f2
Saad Asif <saadasif2121@gmail.com>
2020-05-31 14:39:37 -0700

    new changes

--- client/src/components/pages/Accounts/Singal-Account/SingalAccount.js
+++ client/src/components/pages/Accounts/Singal-Account/SingalAccount.js
@@ -6,6 +6,9 @@ import { isAuth } from '../../../../helpers/auth';
 import Navbar from '../../../layouts/Navbar/Navbar';
 
 const SingalAccount = ({ history }) => {
+  const [userProfile, setUserProfile] = useState('');
+  const [userPosts, setUserPosts] = useState('');
+
   useEffect(() => {
     // gets the id from url
     const documentID = queryString.parse(history.location.search);
@@ -18,11 +21,25 @@ const SingalAccount = ({ history }) => {
       .catch((error) => {
         console.log(error);
       });
+
+    axios
+      .get(`/api/account/${documentID.id}`)
+      .then((response) => {
+        setUserProfile(response.data.userProfile);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }, []);
 
   return (
-    <div>
+    <div className='container'>
       <Navbar showArrow={true} arrowLink='/accounts' />
+
+      {/* Account Info */}
+      <div style={{ marginTop: '10rem' }} className='account-info-container'>
+        {userProfile && userProfile.name}
+      </div>
     </div>
   );
 };
